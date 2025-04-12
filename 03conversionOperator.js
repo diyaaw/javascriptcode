3ccc60dd04716550f7817a213a68ec1dd29dcfe2
diyaaw <diya.wadhwa24@gmail.com>
2025-04-12 11:50:20 +0000

    conversionOfDatatypes

---
+++ 03conversionOperator.js
@@ -0,0 +1,62 @@
+// hume browser jyda tar string values hi deta haii
+let score=null
+//   console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// 
+// "33"=> 33 
+// "33abc"=> NaN(typeof Nan is number)
+// true=> 1
+// false=>0
+// undefined=> Nan
+// null=>0
+
+let isLoggedIn= ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof(booleanIsLoggedIn))
+
+//1=> true 0=>false
+//"" => false
+//" diya " => true
+
+let a=23
+let sam= String(a)
+// console.log(typeof (sam))
+// console.log(sam)
+/****************OPERATIONS***************/
+// let value=3
+// let negValue =-value
+// console.log(negValue)
+
+// arithimetic operations=>
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) 2 raised to the power 3
+// console.log(2/3)
+// console.log(2%3) gives remainder
+
+//  let str1="Hello"
+//  let str2=" Diyaaaaa <3"
+//  let str3= str1+str2
+//  console.log(str3);
+
+//  console.log("1" + 2)//12
+//  console.log(1+ "2")//12
+//  console.log("1"+2+2)// 122
+//  console.log(1+2+"2") // 32
+
+console.log(+true)// + operator is used here for assigning the numeric value for true 
+console.log(+"")// here + operator is used for assigning the numeric value to the string value
+let gameCounter=100;
+++gameCounter
+console.log(gameCounter)
+// ++a this is preincrement in this first the value is incremented and then assigned to the respective variable
+// a++ this is post increment in this first the value is assigned and then the value is incremented
