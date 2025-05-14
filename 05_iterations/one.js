73e74d02506f9866224f594e6882b312863f1fa1
diyaaw <diya.wadhwa24@gmail.com>
2025-05-14 17:02:57 +0000

    commit

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -7,11 +7,36 @@ for (let Diya = 0; Diya < 10; Diya++) {
 // console.log(element);
 }
 for(let i=0;i<=10;i++){
-    console.log(`Outer loop value : ${i}`);
+    //console.log(`Outer loop value : ${i}`);
     for(let j=0;j<=10;j++){
-        // console.log(`Inner loop value ${j} and outer  loop ${i}`);
+       //  console.log(`Inner loop value ${j} and outer  loop ${i}`);
 
         // console.log(i +'*' +j +' = ' +i*j )
 
     }
 }
+
+let myArray= ["flash", "batman", "superman"]
+   // console.log(myArray.length);
+for(let index=0;index<=myArray.length;index++){
+    const element= myArray[index];
+   // console.log(element);
+}
+
+//break and continue
+
+for (let i = 0; i < 20; i++) {
+    if (i== 5) {
+        console.log("detected 5");
+        continue;
+        
+    }
+console.log(`value of i is ${i}`);
+   
+}
+
+//continue-ek baar skip krdo
+//break mtlb full break
+
+
+
